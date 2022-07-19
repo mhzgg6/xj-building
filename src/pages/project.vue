662bf2df3588ef1b9da9072757ccc989e2e3c6cb
mhz <1695934841@qq.com>
2022-07-19 23:48:05 +0800

    project page

--- src/pages/project.vue
+++ src/pages/project.vue
@@ -1,18 +1,29 @@
 <script setup>
-// const { t } = useI18n()
-// const getAssetsImages = (name, type) => {
-//   return new URL(`/src/imgs/${name}.${type}`, import.meta.url).href
-// }
+const { t } = useI18n()
+const getAssetsImages = (name, type) => {
+  return new URL(`/src/imgs/${name}.${type}`, import.meta.url).href
+}
 // const imgs = [
 //   getAssetsImages('blue', 'jpg'),
 //   getAssetsImages('xj', 'png'),
 // ]
 
-const times = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, '按时间']
-console.log(times);
+// const times = reactive([2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, '按时间'])
+const times = ref([2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, '按时间'])
+// const projects = reactive([
+//   {
+//     time: 2020,
+//     name: '像个利拉',
+//     img: getAssetsImages('blue', 'jpg'),
+//   },
+//   {
+//     time: 2020,
+//     name: '像个利拉',
+//     img: getAssetsImages('xj', 'png'),
+//   }
+// ])
 </script>
 
-// 项目
 <template>
   <div
     flex
@@ -33,6 +44,18 @@ console.log(times);
     >
       {{ time }}
     </a> -->
+  </div>
+
+  <div
+    flex
+    max-w-1180px
+    ma-a
+    pl-20px
+    pr-20px
+  >
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
