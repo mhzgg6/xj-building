3520c5eb176d9a1b665a955ddddc775ec5b19d51
WindyNanzi <hanqiao1024@163.com>
2022-07-19 10:48:56 +0800

    chore: 移除多余 Header

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -3,6 +3,5 @@
   <!-- <main class="px-4 py-10 text-gray-700 dark:text-gray-200"> -->
   <main class="text-gray-700 dark:text-gray-200 w-100% h-100vh">
     <RouterView />
-    <Header />
   </main>
 </template>
